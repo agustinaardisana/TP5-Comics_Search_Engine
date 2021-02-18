25f9cc1de6e699f27c9614f2f5983ac419ed48f2
agustinaardisana <agustinaardisana@gmail.com>
2021-02-17 16:36:09 -0800

    fetchInfo() working

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const searchButton = document.querySelector(".search__button");
 //Other Variables
 const baseURL = "https://gateway.marvel.com/v1/public/";
 const myApiKey = "09f672ea66d3144befdf6f7fc2796c10";
-let type = "comic";
+let type = "comics";
 let sort = "title";
 const itemsPerPage = 20;
 const currentPage = 0;
@@ -20,27 +20,22 @@ let url = ``;
 const createURL = (queryParamType, queryParamSort) => {
   queryParamType = type;
   queryParamSort = sort;
-  console.log(queryParamType, queryParamSort);
 
   url = `${baseURL + queryParamType}?apikey=${myApiKey}&offset=${
     currentPage * itemsPerPage
   }&orderBy=${queryParamSort}`;
-  console.log(url);
   return url;
 };
 createURL();
-console.log(url);
 
 const fetchInfo = (url) => {
-  console.log("url");
-
   fetch(url)
     .then((data) => data.json())
     .then((info) => {
       createComicsCards(info);
     });
 };
-fetchInfo();
+fetchInfo(url);
 
 const createComicsCards = (info) => {
   comicsSection.innerHTML = "";
@@ -56,47 +51,3 @@ const createComicsCards = (info) => {
         </article>`);
   });
 };
-
-const createCharactersCards = () => {
-  charactersSection.innerHTML = "";
-  info.data.results.map((character) => {
-    return (charactersSection.innerHTML += `<article class="card--container__character">
-          <div class="img--container__character">
-            <img class="img__character" src="imgs/AVENGERS-FALCON-NEEDLE.png" />
-          </div>
-          <div class="title--container__character">
-            <h3 class="title__character">3-D Man</h3>
-          </div>
-        </article>`);
-  });
-};
-
-// const updateSortByOptions = () => {
-//   console.log("entre a sort by");
-//   queryParamType = typeFilterSelect.value;
-//   if (queryParamType === "characters") {
-//     sortBySelectForComics.classList.add("hidden");
-//     sortBySelectForCharacters.classList.remove("hidden");
-//     queryParamSort = sortBySelectForCharacters.value;
-//   } else {
-//     queryParamSort = sortBySelectForComics.value;
-//   }
-//   console.log(queryParamType, queryParamSort);
-//   updateSearch(queryParamType, queryParamSort);
-// };
-
-// const updateSearch = (queryParamType, queryParamSort) => {
-//   searchButton.onclick = () => {
-//     console.log(queryParamType, queryParamSort);
-
-//     createURL(queryParamType, queryParamSort);
-//     fetchInfo();
-//   };
-//   createURL(queryParamType, queryParamSort);
-// };
-// updateSearch();
-
-// const checkParameters = () => {
-//   updateSortByOptions();
-//   typeFilterSelect.onchange = () => updateSortByOptions();
-// };
